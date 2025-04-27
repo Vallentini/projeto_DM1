b2594d6ab6afe0367c31e38fdf4a7e9b27f401fb
vallentini <vallentinigabrielli.silva@gmail.com>
2025-04-27 14:55:42 -0300

    script.js

--- script.js
+++ script.js
@@ -14,77 +14,85 @@ createApp({
                 title: 'Homer',
                 image:'assets/images/funkoHomer.png',
                 description: 'Funko Pop',
-                inStock: 0
-
-
+                inStock: 0,
+                price: 'R$150,00'
             },
             {
                 title: 'Skeleton Margie',
                 image:'assets/images/funkopopMargeSimpson.webp',
                 description: 'Funko Pop',
-                inStock: 0
+                inStock: 0,
+                price: 'R$120,00'
             },
             {
                 title: 'Maggie Simpson',
                 image: 'assets/images/funkoMaggieSimpson.webp',
                 description: 'Funko Pop',
-                inStock: 30
-
-
+                inStock: 30,
+                price: 'R$230,00'
             },
             {
                title: 'Bart Simpson',
                image: 'assets/images/funkopopBart.jpg',
                description: 'Funko Pop',
-               inStock: 2
+               inStock: 2,
+               price: 'R$190,00'
             },
             {
                  title: 'Milhouse Fallout Boy',
                  image:'assets/images/funkopopMilhouseFalloutBoy.png',
                  description: 'Funko Pop',
-                 inStock: 30
+                 inStock: 30,
+                 price: 'R$160,00'
             },
             {
                 title: 'Hugo Simpson',
                 image: 'assets/images/funkopopHugoSinpson.jpg',
                 description: 'Funko Pop',
-                inStock: 3
+                inStock: 3,
+                price: 'R$200,00'
             },
             {
                 title: 'Sideshow Bob',
                 image:'assets/images/funkopopSideshowBob.webp',
                 description: 'Funko Pop',
-                inStock: 10
+                inStock: 10,
+                price: 'R$190,00'
             },
             {
                 title:'Ralph Wiggum',
                 image:'assets/images/funkopopRalphWiggum.webp',
                 description: 'Funko Pop',
-                inStock: 0
+                inStock: 0,
+                price: 'R$180,00'
             },
             {
                 title:'Deep Space Homer',
                 image:'assets/images/deepspacehomer.jfif',
                 description: 'Funko Pop',
-                inStock: 0
+                inStock: 0,
+                price: 'R$250,00'
             },
             {
                 title:'Mr. Sparkley',
                 image:'assets/images/mrsparkley.jfif',
                 description: 'Funko Pop',
-                inStock: 0
+                inStock: 0,
+                price: 'R$150,00'
             },
             {
                 title:'Evil Homer',
                 image:'assets/images/evilhomer.jfif',
                 description: 'Funko Pop',
-                inStock: 7
+                inStock: 7,
+                price: 'R$170,00'
             },
             {
                 title:'Lisandra',
                 image:'assets/images/lisandra.jfif',
                 description: 'Funko Pop',
-                inStock: 20
+                inStock: 20,
+                price: 'R$50,00'
             }
 
         ]);
